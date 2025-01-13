15772f93a76ed52743ae2e0a85dc8645d4d3f6a9
玄晖allen <sunboyallen@163.com>
2025-01-13 18:45:14 +0800

    Site updated: 2025-01-13 18:45:11

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1736758065552";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1736765108894";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
